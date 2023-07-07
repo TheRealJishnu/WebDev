0dc187e41a673b12d5e0fb60a221ef2aaf6272f2
TheRealJishnu <93379644+TheRealJishnu@users.noreply.github.com>
2023-07-07 12:18:51 +0530

    Update scriptt.js

--- JS/scriptt.js
+++ JS/scriptt.js
@@ -1,4 +1,24 @@
 document.write("Welcome to JavaScript")
 function message(){
     alert("Is this working?")
+    inp()
+    // printA()
+    // printB()
+    
 }
+function printA(){
+    alert("Print a")
+}
+
+function printB(){
+    alert("Print b")
+    
+}
+function inp(){
+    var a = parseInt(prompt("Enter the value of a : "))
+    var b = parseInt(prompt("Enter the value of b : "))
+    alert(a+b)
+}
+
+// function add(a, b)
+

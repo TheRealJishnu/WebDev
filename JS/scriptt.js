ae24e7e9cc7aead92fb0f3e932101bb406a3ddab
TheRealJishnu <93379644+TheRealJishnu@users.noreply.github.com>
2023-07-14 11:31:59 +0530

    14th July class

--- JS/scriptt.js
+++ JS/scriptt.js
@@ -1,10 +1,14 @@
 document.write("Welcome to JavaScript")
+let x = "College"
+
+
+
 function message(){
     alert("Is this working?")
-    inp()
-    // printA()
-    // printB()
-    
+    // inp()
+    printA()
+    printB()
+    // NewFunction()
 }
 function printA(){
     alert("Print a")
@@ -14,11 +18,19 @@ function printB(){
     alert("Print b")
     
 }
-function inp(){
-    var a = parseInt(prompt("Enter the value of a : "))
-    var b = parseInt(prompt("Enter the value of b : "))
-    alert(a+b)
+
+function NewFunction(){
+    let y = 30
+    // console.log(x + y)
+    alert(x + y)
+    document.getElementById("Tab1").innerHTML = 45+55
 }
+
+// function inp(){
+//     var a = parseInt(prompt("Enter the value of a : "))
+//     var b = parseInt(prompt("Enter the value of b : "))
+//     alert(a+b)
+// }
 
 // function add(a, b)
 

5bf08f61d399c90e3875497d7c1729692d94d8b7
TheRealJishnu <93379644+TheRealJishnu@users.noreply.github.com>
2023-07-14 12:21:19 +0530

    14th July Class

--- JS/scriptt.js
+++ JS/scriptt.js
@@ -24,6 +24,32 @@ function NewFunction(){
     // console.log(x + y)
     alert(x + y)
     document.getElementById("Tab1").innerHTML = 45+55
+}document.write("Welcome to JavaScript")
+let x = "College"
+
+
+
+function message(){
+    alert("Is this working?")
+    // inp()
+    printA()
+    printB()
+    // NewFunction()
+}
+function printA(){
+    alert("Print a")
+}
+
+function printB(){
+    alert("Print b")
+    
+}
+
+function NewFunction(){
+    let y = 30
+    // console.log(x + y)
+    alert(x + y)
+    document.getElementById("Tab1").innerHTML = 45+55
 }
 
 // function inp(){
@@ -52,4 +78,54 @@ function Get3(){
     }
 
     window.alert("Max number is : " + max)
+}
+
+function LOOP(){
+    for(let i = 10; i >= 1; i--){
+        alert(i + "\n")
+    }
+}
+
+function ArrayFunc(){
+    var arr = {id:102, nam:"Unnamed", roll:-1}
+    alert(arr.id + " " + arr.nam)
+}
+
+// function inp(){
+//     var a = parseInt(prompt("Enter the value of a : "))
+//     var b = parseInt(prompt("Enter the value of b : "))
+//     alert(a+b)
+// }
+
+// function add(a, b)
+
+function Get3(){
+    var a = parseInt(prompt("Enter First Number : "))
+    var b = parseInt(prompt("Enter Second Number : "))
+    var c = parseInt(prompt("Enter Third Number : "))
+    var max = 0
+    if(a>b && a>c){
+        max = a;
+    }
+    else{
+        if(b>c){
+            max = b
+        }
+        else{
+            max = c
+        }
+    }
+
+    window.alert("Max number is : " + max)
+}
+
+function LOOP(){
+    for(let i = 10; i >= 1; i--){
+        alert(i + "\n")
+    }
+}
+
+function ArrayFunc(){
+    var arr = {id:102, nam:"Unnamed", roll:-1}
+    alert(arr.id + " " + arr.nam)
 }
